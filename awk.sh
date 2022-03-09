2d2f987ea12f5182a88d871e56c190c1e8647a2a
cl340266909 <cl340266909@163.com>
2022-03-09 23:03:10 +0800

    update some note

--- awk.sh
+++ awk.sh
@@ -13,3 +13,11 @@
   $NF 最后一列
   $1,$2...$n 文件中每行以间隔符号分隔的不同字段
   FNR NR 行号   record
+
+  #匹配打印
+  awk '/root/{print $0}' /etc/passwd      //$0指代整行
+  awk '/root/' /etc/passwd   //也可以实现上述功能
+  awk 'NR==1,NR==5{print $0}' /etc/passwd   //打印第1到5行
+
+  awk 'BEGIN{...}{语句}END{...}' /etc/passwd  //BEGIN在程序开始前执行，END所有文件处理完后执行
+
--- sed.sh
+++ sed.sh
@@ -2,4 +2,14 @@
 
   sed：stream editor 流编辑器，一行一行读取文件，并按照进行处理
 
+  sed [选项] '处理动作' 文件名
+  选项:
+  -i 修改文件
+  -n 只显示匹配的行，默认会显示所有航
+
+  处理动作：
+  p 打印
+  sed -n '/root/p' /etc/passwd
+  sed -n '1,5p' /etc/passwd
+
   sed -i 's/ed/bb/g' test.txt
