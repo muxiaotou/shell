e4953d266e9606eb3447ca45c4cfe78bdd5481c9
muxiaotou <cl340266909@163.com>
2022-03-09 20:04:09 +0800

    add some note 20220309

--- awk.sh
+++ awk.sh
@@ -1,6 +1,13 @@
-  awk：
-  -F  定义字符的分隔符，默认的分隔符是空格
+  https://mp.weixin.qq.com/s/rIvOa5yvXFCAWiidxFz_ug
 
+  awk：逐行扫描文件进行处理
+  awk 选项 ”命令部分“ 文件
+
+  常用选项：
+  -F  定义字符的分隔符，默认的分隔符是空格
+  -v  定义变量并赋值
+
+  命令部分：
   awk内部相关变量：
   NF 当前记录的字段数(列数)  awk '{print NF}' test.txt   field
   $NF 最后一列
--- sed.sh
+++ sed.sh
@@ -1,3 +1,5 @@
+  https://mp.weixin.qq.com/s/DMC8Ba8GgqZ1rKAIJ-66hA
+
   sed：stream editor 流编辑器，一行一行读取文件，并按照进行处理
 
   sed -i 's/ed/bb/g' test.txt
